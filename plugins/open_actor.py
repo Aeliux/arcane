401d117e37a8f51cffaafec7cae41f46eb13155d
Aeliux <itsaeliux@gmail.com>
2023-07-03 20:55:52 +0330

    open_actor: add blast hook
    Signed-off-by: Aeliux <itsaeliux@gmail.com>

--- plugins/open_actor.py
+++ plugins/open_actor.py
@@ -68,6 +68,8 @@ class OpenActor(babase.Plugin):
             bomb.Bomb.handlemessage = self.replace(
                 "bomb_handler", bomb.Bomb.handlemessage
             )
+            
+            bomb.Blast.__init__ = self.replace("blast", bomb.Blast.__init__)
 
             powerupbox.PowerupBox.__init__ = self.replace(
                 "powerupbox", powerupbox.PowerupBox.__init__
