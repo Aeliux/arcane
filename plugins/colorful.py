8178f240451d18d4de7fe4cd9bdf79085dbb7a48
Aeliux <itsaeliux@gmail.com>
2023-07-03 21:04:28 +0330

    add colorful mod
    Signed-off-by: Aeliux <itsaeliux@gmail.com>

---
+++ plugins/colorful.py
@@ -0,0 +1,29 @@
+# ba_meta require api 8
+
+import babase as ba
+import bascenev1 as bs
+
+from bascenev1lib.actor.bomb import Blast
+from open_actor import OpenActor, safe_inject_task
+
+import random
+
+
+def colored_blast(self: Blast, **kwargs):
+    scorchRadius = self.radius
+    s = bs.newnode('scorch',attrs={'position':self.node.position, 'size':scorchRadius*0.5,'big':(self.blast_type == 'tnt')})
+    s2 = bs.newnode('scorch',attrs={'position':self.node.position, 'size':scorchRadius*0.5,'big':(self.blast_type == 'tnt')})
+    s3 = bs.newnode('scorch',attrs={'position':self.node.position,'size':scorchRadius*0.5,'big':(self.blast_type == 'tnt')})
+    if self.blast_type == 'ice': s.color = s2.color = s3.color =(1,1,1.5)
+    else: s.color = s2.color = s3.color = (random.random(),random.random(),random.random())
+
+    bs.animate(s,"presence",{3000:1, 13000:0})
+    bs.animate(s2,"presence",{3000:1, 13000:0})
+    bs.apptimer(13000,s.delete)
+    bs.apptimer(13000,s2.delete)
+
+
+# ba_meta export plugin
+class Colorful(ba.Plugin):
+    def on_app_running(self):
+        safe_inject_task("colorful_blast", "blast", colored_blast, "late")
